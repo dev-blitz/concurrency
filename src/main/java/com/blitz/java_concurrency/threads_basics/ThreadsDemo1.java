de40c8cd02937d0a6dba949037a9634f7da4a806
dev-blitz <abhinab.duttagupta@gmail.com>
2026-03-13 08:59:51 +0530

    added demo for threads * added code for creating thread with Runnable interface

---
+++ src/main/java/com/blitz/java_concurrency/threads_basics/ThreadsDemo1.java
@@ -0,0 +1,28 @@
+package com.blitz.java_concurrency.threads_basics;
+
+public class ThreadsDemo1 {
+    public static void main(String[] args) {
+        /*
+         * two ways to create threads:
+         * 1. by implementing Runnable interface and create a Thread object with runnable instance
+         * 2. by extending Threads class to create a thread object
+         */
+
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 5; i++) {
+                    System.out.println("i: " + i);
+                    try {
+                        Thread.sleep(100);
+                    } catch(Exception exp) {
+                        System.out.println(exp.getClass().getSimpleName() + " was triggered => " + exp.getMessage());
+                    }
+                }
+            }
+        };
+
+        Thread thread = new Thread(runnable);
+        thread.start();
+    }
+}
